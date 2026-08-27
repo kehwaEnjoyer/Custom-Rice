a6bf7af92242887032edb7ccfe4781d5b0ea14e8
kehwaEnjoyer <yahyasmughal@gmail.com>
2026-08-27 12:52:59 +0500

    Add files via upload
    new lua config for hyprland.

---
+++ hypr/luaConf/programes.lua
@@ -0,0 +1,6 @@
+--Set programs that you use
+terminal = "kitty"
+fileManager = "dolphin"
+menu = "wofi --show drun"
+browser = "firefox"
+screenshot = 'grim -g "$(slurp)" - | wl-copy'
---
+++ hypr/luaConf/windowAndWorkspaces.lua
@@ -0,0 +1,48 @@
+-- See https://wiki.hypr.land/Configuring/Basics/Window-Rules/
+-- and https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
+
+-- Example window rules that are useful
+
+local suppressMaximizeRule = hl.window_rule({
+    -- Ignore maximize requests from all apps. You'll probably like this.
+    name  = "suppress-maximize-events",
+    match = { class = ".*" },
+
+    suppress_event = "maximize",
+})
+-- suppressMaximizeRule:set_enabled(false)
+
+hl.window_rule({
+    -- Fix some dragging issues with XWayland
+    name  = "fix-xwayland-drags",
+    match = {
+        class      = "^$",
+        title      = "^$",
+        xwayland   = true,
+        float      = true,
+        fullscreen = false,
+        pin        = false,
+    },
+
+    no_focus = true,
+})
+
+-- Layer rules also return a handle.
+-- local overlayLayerRule = hl.layer_rule({
+--     name  = "no-anim-overlay",
+--     match = { namespace = "^my-overlay$" },
+--     no_anim = true,
+-- })
+-- overlayLayerRule:set_enabled(false)
+
+-- Hyprland-run windowrule
+hl.window_rule({
+    name  = "move-hyprland-run",
+    match = { class = "hyprland-run" },
+
+    move  = "20 monitor_h-120",
+    float = true,
+})
+
+--speacial drop down terminal
+hl.workspace_rule({ workspace = "special:quake", on_created_empty= terminal ,gaps_out=50})
