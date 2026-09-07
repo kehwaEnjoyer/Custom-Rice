ad50c7b8acc018cbe7adad218c263d889a16cf86
kehwaEnjoyer <yahyasmughal@gmail.com>
2026-09-07 14:27:41 +0500

    changed to lua format for hyprland and some other changes
    customized wofi 
    added folder for internal scripts
    switched to ashell from waybar

--- hypr/luaConf/programes.lua
+++ hypr/luaConf/programes.lua
@@ -1,6 +1,6 @@
 --Set programs that you use
 terminal = "kitty"
 fileManager = "dolphin"
-menu = "wofi --show drun"
+menu = "$HOME/.config/scripts/wofi-rand"
 browser = "firefox"
 screenshot = 'grim -g "$(slurp)" - | wl-copy'
