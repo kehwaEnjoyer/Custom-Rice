db77666cd6a8082fdbc61cc1918970209fc7e114
kehwaEnjoyer <yahyasmughal@gmail.com>
2026-09-10 10:45:49 +0500

    fixed mirror projection, (untested)

--- hypr/luaConf/moniters.lua
+++ hypr/luaConf/moniters.lua
@@ -1,12 +1,15 @@
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
 hl.monitor({
-    output   = "",
+    output   = "eDP-1",
     mode     = "preferred",
     position = "auto",
     scale    = "1",
 })
 
 hl.monitor({
-    output = "HDMI-A-1",
+    output = "",
+    mode     = "preferred",
+    position = "auto",
+    scale    = "1",
     mirror = "eDP-1"
 })
